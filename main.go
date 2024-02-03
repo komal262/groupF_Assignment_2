e2382cc7f27d2b33d45fb797c3f7416f0b5fe173
Navneet Kaur <geek.navneetkaur@gmail.com>
2024-02-02 21:00:22 -0500

    added function two to generate a fibonacci series.

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ func main() {
 	fmt.Println("::DHARMIK DARJI functions's output::")
 	fmt.Println(base, "raised to the power of", exponent, "is", result)
 	fmt.Println("")
+
+	//Testing Fibonacci Series Function (Function Two)
+	fmt.Println("Output of the function created by Navneet Kaur ")
+	fmt.Println(Fibonacci(8))
 }
 
-// function Two
+// function One
 // Created by dharmik darji
 // Calculates and returns the result of raising a base to a given exponent
 // INPUT: base - the base value to be raised
@@ -28,4 +32,20 @@ func power(base, exponent int) int {
 		result *= base
 	}
 	return result
+}
+
+// function Two
+// Created by Navneet Kaur
+// Function generates a Fibonacci series up to n terms
+// Input: n (int)
+// Output: fibSer ([]int)
+func Fibonacci(n int) []int {
+	fibSer := make([]int, n)
+	fibSer[0], fibSer[1] = 0, 1
+
+	for i := 2; i < n; i++ {
+		fibSer[i] = fibSer[i-1] + fibSer[i-2]
+	}
+
+	return fibSer
 }
