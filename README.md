5d5ad88af0d8be6b9f34d3ac45998696906a3a08
dharmik darji <48235919+Dharmikdarji@users.noreply.github.com>
2024-02-02 20:14:31 -0500

    Added power function(Dharmik)

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This Go program demonstrates several functions created by different contributors. Each function performs a specific task and is tested independently.
 
+**Dharmik Darji**
+    - Function: `power`
+    - Calculates and returns the result of raising a base to a given exponent.
+    - Input: Base (integer), Exponent (integer)
+    - Output: Result (integer) - the result of base raised to the exponent
+
--- main.go
+++ main.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello form group F")
+	// Test the power function
+	// Created by Dharmik Darji
+	base, exponent := 2, 3
+	result := power(base, exponent)
+	fmt.Println("::DHARMIK DARJI functions's output::")
+	fmt.Println(base, "raised to the power of", exponent, "is", result)
+	fmt.Println("")
+}
+
+// function Two
+// Created by dharmik darji
+// Calculates and returns the result of raising a base to a given exponent
+// INPUT: base - the base value to be raised
+//
+//	exponent - the exponent value to which the base is raised
+//
+// OUTPUT: result (integer) - the result of base raised to the exponent
+func power(base, exponent int) int {
+	result := 1
+	for i := 0; i < exponent; i++ {
+		result *= base
+	}
+	return result
 }
